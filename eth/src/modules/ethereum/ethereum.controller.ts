9a1bd0cfc7033b9cd444c7884cfea99041271101
Milex <mishin-lex@mail.ru>
2021-09-19 14:42:58 +0300

    feat: add most changed wallet api

--- eth/src/modules/ethereum/ethereum.controller.ts
+++ eth/src/modules/ethereum/ethereum.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { EthereumService } from './ethereum.service';
 
 @Controller('ethereum')
-export class EthereumController {}
+export class EthereumController {
+  constructor(private readonly ethereumService: EthereumService) {}
+
+  @Get('/most-changed-wallet')
+  getMostChangedWallet() {
+    return this.ethereumService.getMostChangedWallet(100);
+  }
+}
--- eth/src/modules/ethereum/ethereum.service.ts
+++ eth/src/modules/ethereum/ethereum.service.ts
@@ -6,6 +6,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { etherscan } from '../../config';
 import { wait } from '../../common/helpers/time.helper';
+import { BigIntMath } from '../../common/helpers/bigInt.helper';
 import Block from './entities/block.entity';
 import Transaction from './entities/transaction.entity';
 import { IntervalBetweenCall } from './ethereum.decorators';
@@ -111,5 +112,55 @@ export class EthereumService {
     }
 
     await this.storeBlock(blockInfo, nextBlockNumber);
+  }
+
+  async getMostChangedWallet(blocksCount: number) {
+    const blocksWithTransactions = await this.blockRepository
+      .createQueryBuilder('block')
+      .orderBy('block.id', 'DESC')
+      .take(blocksCount)
+      .leftJoinAndSelect('block.transactions', 'transaction')
+      .getMany();
+
+    const wallets: Record<string, bigint> = {};
+
+    blocksWithTransactions.forEach((block) => {
+      block.transactions.forEach((transaction) => {
+        const transactionValue = BigInt(transaction.value);
+
+        if (wallets[transaction.from] === undefined) {
+          wallets[transaction.from] = BigInt(transactionValue);
+        } else {
+          wallets[transaction.from] += transactionValue;
+        }
+
+        if (!transaction.to) {
+          return;
+        }
+
+        if (wallets[transaction.to] === undefined) {
+          wallets[transaction.to] = -transactionValue;
+        } else {
+          wallets[transaction.to] -= transactionValue;
+        }
+      });
+    });
+
+    const mostChangedWalletAddress = Object.entries(wallets).reduce(
+      (mostChangedWallet, [walletAddress, walletDelta]) => {
+        const absParam = BigIntMath.abs(walletDelta);
+
+        if (absParam > mostChangedWallet.value) {
+          return { address: walletAddress, value: absParam };
+        }
+        return mostChangedWallet;
+      },
+      { address: '', value: 0n },
+    );
+
+    return {
+      address: mostChangedWalletAddress.address,
+      walletDelta: mostChangedWalletAddress.value.toString(),
+    };
   }
 }
