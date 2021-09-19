05d0088156e16ead8117b8317faec4d33afabc45
Milex <mishin-lex@mail.ru>
2021-09-19 13:47:08 +0300

    fix(transaction-entity): table name and columns meta data

--- eth/src/modules/ethereum/entities/transaction.entity.ts
+++ eth/src/modules/ethereum/entities/transaction.entity.ts
@@ -1,16 +1,22 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import Block from './block.entity';
 
-@Entity('users')
-export class Transaction {
+@Entity('transactions')
+export default class Transaction {
   @PrimaryGeneratedColumn()
   id: string;
 
   @Column()
   from: string;
 
-  @Column()
+  @Column({ nullable: true })
   to: string;
 
-  @Column({ type: 'bigint' })
+  @Column({
+    // type: 'bytea',
+  })
   value: string;
+
+  @ManyToOne(() => Block, (block) => block.transactions)
+  block: Block;
 }
